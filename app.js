aac3895ca28bd83c624eab0fc3a56fe20a0f8e47
dolev146 <62290677+dolev146@users.noreply.github.com>
2021-09-01 12:05:27 +0300

    second screen started

---
+++ app.js
@@ -0,0 +1,20 @@
+// we have three screens
+// first is home screen with 2 buttons 
+
+const game = document.querySelector(".gameArea");
+const playBtn = document.querySelector(".btn-primary");
+const InstructionsBtn = document.querySelector(".btn-info");
+
+playBtn.addEventListener("click", () => {
+  // inserting the play screen
+    
+  game.innerHTML = "<button>dolev</button>";
+});
+
+
+
+
+
+
+
+
